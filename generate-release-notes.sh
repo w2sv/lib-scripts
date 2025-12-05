e4ab561482b788d83d32233bc30f70ca6ed45a73
w2sv <zangenbergjanek@googlemail.com>
2025-12-05 22:21:10 +0100

    Change generated release notes

--- generate-release-notes.sh
+++ generate-release-notes.sh
@@ -3,28 +3,36 @@ set -e
 
 OUTPUT_FILE="release-notes.txt"
 
-# Fix GitHub URL
+# GitHub URL
 GITHUB_URL=$(git remote get-url origin | sed -E 's#(git@|https://)([^/:]+)[:/](.+)\.git#https://\2/\3#')
 
-# Previous tag
-LAST_TAG=$(git describe --tags --abbrev=0 HEAD^ 2>/dev/null || echo "")
+# Get tags
+LAST_TAG=$(git describe --tags --abbrev=0)
+PREVIOUS_TAG=$(git tag --sort=-creatordate --merged HEAD | sed -n '2p' || echo "")
 
 # Header
-echo "# Changes since last release" > "$OUTPUT_FILE"
+echo "## What's changed" > "$OUTPUT_FILE"
 echo "" >> "$OUTPUT_FILE"
 
-# Commits
-if [ -z "$LAST_TAG" ]; then
+# Get commits
+if [ -z "$PREVIOUS_TAG" ]; then
+    # No previous tag, include all commits
     COMMITS=$(git log --pretty=format:"%h %s")
 else
-    COMMITS=$(git log "$LAST_TAG"..HEAD --pretty=format:"%h %s")
+    COMMITS=$(git log "$PREVIOUS_TAG"..HEAD --pretty=format:"%h %s")
 fi
 
-# Write markdown links
+# Write commits as - msg [hash]
 while IFS= read -r line; do
     HASH=$(echo "$line" | awk '{print $1}')
     MSG=$(echo "$line" | cut -d' ' -f2-)
-    echo "- [$MSG]($GITHUB_URL/commit/$HASH)" >> "$OUTPUT_FILE"
+    echo "- $MSG [$HASH]($GITHUB_URL/commit/$HASH)" >> "$OUTPUT_FILE"
 done <<< "$COMMITS"
+
+# Append full changelog link
+if [ -n "$PREVIOUS_TAG" ]; then
+    echo "" >> "$OUTPUT_FILE"
+    echo "**Full Changelog**: $GITHUB_URL/compare/$PREVIOUS_TAG...$LAST_TAG" >> "$OUTPUT_FILE"
+fi
 
 echo "Release notes written to $OUTPUT_FILE"
